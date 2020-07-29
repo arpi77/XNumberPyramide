090221800531c3c459b94b769a4c9839cb62a9c9
kucserak <kucserak@eset.sk>
2020-07-29 16:14:42 +0200

    Add project files.

---
+++ Numbers/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Numbers
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private Random _random;
+        private MainViewModel _vm;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            //ButtonCircleRenderer.Init();
+        }
+
+        private void Handle_Clicked(object sender, EventArgs e)
+        {
+            _random = new Random();
+            _vm = BindingContext as MainViewModel;
+            var num = GenerateFirstRow();
+
+            _vm.Numbers = String.Join(".", num[1].Select(x => x.ToString()).ToArray());
+
+            for (int i = 1; i <= num.Count; i++)
+            {
+                CalculatePyramid(num, i);
+            }
+
+            _vm.Result = num[_vm.Count].First();
+            _vm.ResultVisibility = false;
+        }
+
+        private Dictionary<int, List<int>> GenerateFirstRow()
+        {
+            var num = new Dictionary<int, List<int>>();
+            var nums = new List<int>();
+            for (var i = 0; i < _vm?.Count; i++)
+            {
+                nums.Add(_random.Next(1, 19));
+            }
+            num.Add(1, nums);
+            return num;
+        }
+
+        private static void CalculatePyramid(Dictionary<int, List<int>> num, int i)
+        {
+            num.TryGetValue(i, out var lastRow);
+            if (lastRow.Count == 1) return;
+            
+            var nextRow = new List<int>();
+            for (int j = 0; j < lastRow.Count - 1; j++)
+            {
+                nextRow.Add(lastRow[j] + lastRow[j + 1]);
+            }
+
+            num.Add(i + 1, nextRow);
+        }
+
+        private void Slider_OnValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            _vm = BindingContext as MainViewModel;
+            _vm.Count = (int)e.NewValue;
+        }
+
+        private void Handle_Clicked2(object sender, EventArgs e)
+        {
+            _vm = BindingContext as MainViewModel;
+            _vm.ResultVisibility = true;
+        }
+    }
+}
---
+++ Numbers/MainViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Numbers.Annotations;
+
+namespace Numbers
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        string _numbers = ".: some random numbers :.";
+        int _count = 6;
+        int _result = 0;
+        private bool _resultVisibility;
+
+        public string Numbers
+        {
+            get => _numbers;
+            set 
+            {
+                _numbers = value;
+                OnPropertyChanged(nameof(Numbers));
+            }
+        }
+
+        public int Count
+        {
+            get => _count;
+            set
+            {
+                _count = value;
+                OnPropertyChanged(nameof(CountText));
+            }
+        }
+
+        public string CountText => Count.ToString();
+
+        public int Result
+        {
+            get { return _result; }
+            set
+            {
+                _result = value;
+                OnPropertyChanged(nameof(Result));
+            }
+        }
+
+        public bool ResultVisibility
+        {
+            get => _resultVisibility;
+            set
+            {
+                _resultVisibility = value;
+                OnPropertyChanged(nameof(ResultVisibility));
+            } 
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
